105ab40bf4466d9d60ee9a7fe03740dd97468ae6
Alan Dipert <alan@dipert.org>
2013-12-04 12:51:49 -0500

    added notes to nanorel

--- src/tailrecursion/stasis/nanorel.clj
+++ src/tailrecursion/stasis/nanorel.clj
@@ -1,3 +1,67 @@
+;;;;;; Experimental analyze, optimize, emit via core.logic for Clojure -> JavaScript
+
+;;; I think relational analyze and emit phases here are a bit
+;;; indulgent, as they should never produce multiple outputs for the
+;;; same input.  I coded them this way to familiarize myself with
+;;; core.logic.  They are perfectly suited to being regular functions.
+
+;;; While analysis and emission needn't be relational, I think the
+;;; optimization process amenes itself to the tools and techniques of
+;;; logic programming.  Unifying an instantiated AST with a pattern
+;;; feels like a natural way to express the applicability of a rewrite
+;;; and to perform the rewrite itself.
+
+;;; Like any compiler's optimization pass, a relational approach boils
+;;; down to a tree search over optimizations and their permutations
+;;; and is still susceptible to divergence/cycles - the production of
+;;; an infinite number of possible programs.  As opposed to how
+;;; optimization passes are usually organized, though, with relations
+;;; one has a model upon which to reason and debug at a higher level.
+
+;;; The optimization pass may be significantly improved with the
+;;; introduction of a representation that grouped programs into
+;;; equivalence classes as described in [1].
+
+;;; With this approach, optimization relations would unify not with
+;;; particular ASTs but with "E-PEGS" representing the set of
+;;; equivalent ASTs, and would thus be more general.  The property
+;;; that multiple semantically equivalent ASTs converge to a single
+;;; E-PEG is known in term rewriting as that of confluence. [2]
+
+;;; I suspect the performance benefit of introducing an E-PEG
+;;; representation is two-fold.
+
+;;; First, the search for the most optimal program is likely to
+;;; produce an acceptable result sooner, because there are fewer
+;;; objects over which to search, as N ASTs were collapsed to 1 E-PEG
+;;; during "peggification".  While the search space may still be
+;;; infinite, the surface of it we are able to practically search
+;;; grows manyfold.
+
+;;; Second, relations over E-PEGs might be "tabled", or memoized.  In
+;;; this way, work is only done when a genuinely distinct program must
+;;; be compiled.  I imagine a smart compiler using these techniques
+;;; to very efficiently incrementally compile programs after parts of
+;;; them are modified.
+
+;;; So, my current thoughts around how to most awesomely write a compiler are:
+
+;;; 1. Write a function, 'analyze', that reads source code and returns AST.
+;;; 2. Write a set of relations, 'peggify', that read AST and return E-PEG - an AST-like structure whose leaves may be multisets of programmatically equivalent AST.
+;;; 3. Write a set of relations, 'optimize', that take E-PEG and return (possibly optimized) E-PEG.
+;;; 4. Write a function, 'select', that takes optimized E-PEGs and ranks them by one or more program-global selection heuristics.
+;;; 4. Write a function, 'emit', that takes the selected E-PEG and produces target code.
+
+;;; 1. http://www.cs.cornell.edu/~ross/publications/eqsat/eqsat_tate_popl09.pdf
+;;; 2. http://en.wikipedia.org/wiki/Confluence_(abstract_rewriting)
+
+;;; Footnotes
+
+;;; I see a strong connection between the ideas of "Equality
+;;; Saturation" and those presented by John Backus in "From function
+;;; level semantics to program transformation and optimization"
+;;; http://dl.acm.org/citation.cfm?id=21859
+
 (ns tailrecursion.stasis.nanorel
   (:require [clojure.core.logic :refer [run* run conde conda == conso lvaro defna
                                         defne matche project fresh featurec
